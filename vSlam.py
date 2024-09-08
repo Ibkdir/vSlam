b70aa4df6fc844c412285eb180264cb9c1d78b32
Ibkdir <139390442+Ibkdir@users.noreply.github.com>
2024-09-07 22:00:26 -0400

    Feature Detection

--- vSlam.py
+++ vSlam.py
@@ -1,29 +1,39 @@
-import os
-import sys
-
 # Trying the experimental Jax-Metal 😎
 import jax
 import jax.numpy as jnp
-
-import open3d as o3d
 import numpy as np
 import cv2 as cv
 import time
 from display import Display
 
 class vSLAM:
-    def __init__(self, vpath='./videos/main.mp4') -> None:
-        self.vpath = vpath
-        
-    def processFrame(self) -> None:
+    def __init__(self, vidname:str ='highway', fd:str='sift') -> None:
+        self.vpath:str = f'./videos/{vidname}.mp4'
+        self.fd = self.create_feature_detector(fd.lower())
+    
+    def create_feature_detector(self, chosen_fd):
+        if chosen_fd == 'orb':
+            return cv.ORB.create()
+        elif chosen_fd == 'sift':
+            return cv.SIFT.create()
+        else:
+            raise ValueError(f"❗ Unsupported feature detector ❗")
+    
+    def detect_features(self, frame):
+        gray= cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+        kp = self.fd.detect(gray, None)
+        frame=cv.drawKeypoints(gray,kp,frame)
+        return frame
+
+    def processVideo(self) -> None:
         vc = cv.VideoCapture(self.vpath)
         if not vc.isOpened():
             print('Error Opening File')
+            
         while vc.isOpened():
             _, frame = vc.read()
             time.sleep(0.1)
-            cv.imshow('Frame', frame)
-            
+            cv.imshow('Frame', self.detect_features(frame))
             key = cv.waitKey(100)
             if key == ord('q'):
                 break
@@ -34,6 +44,5 @@ class vSLAM:
         window = Display()
         window.runTest()
 
-
 if __name__ == '__main__':
-    vSLAM().testOpen3D()
+    vSLAM(vidname='driving').processVideo()
